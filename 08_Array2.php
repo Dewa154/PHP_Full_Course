dfccedc3e61932685654104edf07fc0ff69c7e5b
Dewa154 <dewanandkurrey10@gmail.com>
2024-11-01 11:30:13 +0530

    array 2

---
+++ 08_Array2.php
@@ -0,0 +1,50 @@
+<html>
+    <head><title>Array-2 in PHP</title></head>
+    <body>
+        <center>
+            <?php
+                // $array1 = array("Mon", "Tue", "Wed", "Sat", "vivek");
+                // $array2 = array("Mon", "Wed", "Thus", "Feb", "Tue");
+                // $array3 = array("Mon", "Wed", "Fri", "Jan", "Sat");
+
+                // // $newArray = array_intersect($array1, $array2, $array3);    // Common value find out
+                // $newArray = array_diff($array1, $array2, $array3);    // different value find out in first array
+                // print_r($newArray)
+
+                // $array1 = array("BTech", "MCA", "BTech", "MTech", "MCA", "BCA");
+                // $newArray = array_unique($array1);    // unique value find out
+                // print_r($newArray)
+
+                // function multi($n){
+                //     return $n*5;
+                // }
+
+                // $array1 = array(5, 10, 20, 30, 40, 50);
+                // $newArray = array_map('multi', $array1);
+                // print_r($newArray);
+
+                // function checkEvenAndOdd($n){
+                //     if($n % 2 == 0){
+                //         return "Even";
+                //     } else {
+                //         return "Odd";
+                //     }
+                // }
+
+                // $array1 = array(5, 7, 4, 88, 122, 13);
+                // $newArray = array_map('checkEvenAndOdd', $array1);
+                // print_r($newArray);
+
+
+                // $newArray = range(1, 20);
+                $newArray = range(1, 20, 2);
+                // print_r($newArray);
+
+                for ($i=0; $i < count($newArray); $i++) { 
+                    echo $newArray[$i]."<br>";
+                }
+                    
+            ?>
+        </center>
+    </body>
+</html>
